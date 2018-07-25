99a123dbb688fbd501c0e064527f280fdb56b263
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-25 19:22:09 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -19,7 +19,7 @@
 
 
 # Call your methods without any arguments here
-puts snacks
+# puts snacks
 ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
 def breakfast(food = "coffee")
   "morning is the best time for #{food}"
